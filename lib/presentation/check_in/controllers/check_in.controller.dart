b8956c8560d2c1de0f479285ecfb9d9c2a16f5aa
Dary Kusuma Hardian <darykusuma741@gmail.com>
2025-08-10 16:42:08 +0700

    check in controller

--- lib/presentation/check_in/controllers/check_in.controller.dart
+++ lib/presentation/check_in/controllers/check_in.controller.dart
@@ -1,12 +1,32 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:crm/common/components/custom_loading/custom_loading.controller.dart';
+import 'package:crm/common/helper/my_location.helper.dart';
+import 'package:crm/common/helper/my_snack_bar.dart';
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class CheckInController extends GetxController {
-  //TODO: Implement CheckInController
+  final Rx<Completer<GoogleMapController>> ctrGoogleMap = Rx(Completer<GoogleMapController>());
+  final Rx<CameraPosition> initialCameraPosition = Rx(CameraPosition(
+    target: LatLng(-6.174692567487111, 106.78966208650647),
+    zoom: 15.4746,
+  ));
+  final Rx<Map<MarkerId, Marker>> markers = Rx(<MarkerId, Marker>{});
+  final Rxn<LocationData?> myLocationData = Rxn();
+  final CustomLoadingController ctrLoading = Get.find<CustomLoadingController>();
+  RxBool isVisible = false.obs;
+  final Rxn<File?> photo = Rxn();
 
-  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
+
+    Future.delayed(Duration(milliseconds: 500), () {
+      isVisible.value = true;
+    });
   }
 
   @override
@@ -19,5 +39,85 @@ class CheckInController extends GetxController {
     super.onClose();
   }
 
-  void increment() => count.value++;
+  void onMapCreated(GoogleMapController controller) {
+    ctrGoogleMap.value.complete(controller);
+    onTapGetMyLocation();
+  }
+
+  void onTapGetMyLocation() async {
+    try {
+      ctrLoading.isLoading.value = true;
+
+      myLocationData.value = await MyLocationHelper.getMyLocation();
+
+      if (myLocationData.value?.latitude == null || myLocationData.value?.longitude == null) throw 'No Location Data';
+
+      addMarker(LatLng(myLocationData.value!.latitude!, myLocationData.value!.longitude!));
+      // address.value = await LocationService().getAddress(myLocationData.value!.latitude!, myLocationData.value!.longitude!);
+
+      final GoogleMapController controller = await ctrGoogleMap.value.future;
+      CameraPosition kLake = CameraPosition(
+        target: LatLng(myLocationData.value!.latitude!, myLocationData.value!.longitude!),
+        zoom: 16.8,
+      );
+
+      await controller.animateCamera(CameraUpdate.newCameraPosition(kLake));
+    } catch (e) {
+      MySnackBar.error(Get.context!, title: e.toString());
+    } finally {
+      ctrLoading.isLoading.value = false;
+    }
+  }
+
+  void addMarker(LatLng position) {
+    final MarkerId markerId = MarkerId('My Location');
+    final Marker marker = Marker(
+      markerId: markerId,
+      position: position,
+      onTap: () {},
+    );
+
+    markers.update((e) {
+      markers.value[markerId] = marker;
+    });
+  }
+
+  void onTapBack() async {
+    Get.back();
+  }
+
+  void onSubmit() async {
+    // if (myLocationData.value?.latitude == null || myLocationData.value?.longitude == null) {
+    //   MySnackBar.error(Get.context!, title: "Lokasi tidak terdeteksi");
+    //   return;
+    // }
+    // if (photo.value == null) {
+    //   MySnackBar.error(Get.context!, title: "Photo wajib di isi");
+    //   return;
+    // }
+
+    // ctrLoading.isLoading.value = true;
+    // CheckInOutModel? result;
+    // if (ctrCheckInMain.lastCheckInOut.value?.logTimeIn == null) {
+    //   result = await ctrCheckInMain.checkInProcess(
+    //     myLocationData.value!.latitude!,
+    //     myLocationData.value!.longitude!,
+    //     photo: photo.value!,
+    //   );
+    // } else {
+    //   result = await ctrCheckInMain.checkOutProcess(
+    //     myLocationData.value!.latitude!,
+    //     myLocationData.value!.longitude!,
+    //     photo: photo.value!,
+    //   );
+    // }
+    // if (result != null) {
+    //   Get.back();
+    //   MySnackBar.success(Get.context!, title: "Sukses", subTitle: "selamat kamu berhasil absen");
+    //   ctrLoading.isLoading.value = true;
+    //   await ctrCheckInMain.getLastCheckIn();
+    //   ctrLoading.isLoading.value = false;
+    // }
+    // ctrLoading.isLoading.value = false;
+  }
 }
--- lib/presentation/face_scan/face_scan.screen.dart
+++ lib/presentation/face_scan/face_scan.screen.dart
@@ -1,24 +1,72 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-
+import 'package:crm/common/components/custom_scaffold.dart';
+import 'package:crm/presentation/face_scan/button/face_scan.button.dart';
+import 'package:crm/presentation/face_scan/navigation/face_scan.navigation.dart';
+import 'dart:math' as math;
 import 'controllers/face_scan.controller.dart';
 
 class FaceScanScreen extends GetView<FaceScanController> {
   const FaceScanScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('FaceScanScreen'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'FaceScanScreen is working',
-          style: TextStyle(fontSize: 20),
+    return Obx(() {
+      return CustomScaffold(
+        backgroundColor: Colors.black,
+        body: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Stack(
+            children: [
+              if (controller.cameraController.value?.value.previewSize != null && controller.picture.value == null)
+                Positioned.fill(
+                  child: Transform(
+                    alignment: Alignment.center,
+                    transform: controller.frontCamera.value ? Matrix4.rotationY(math.pi) : Matrix4.rotationY(0), // mirror front camera
+                    child: FittedBox(
+                      fit: BoxFit.cover,
+                      child: SizedBox(
+                        width: controller.cameraController.value!.value.previewSize!.height,
+                        height: controller.cameraController.value!.value.previewSize!.width,
+                        child: CameraPreview(controller.cameraController.value!),
+                      ),
+                    ),
+                  ),
+                ),
+              // if (controller.picture.value != null)
+              //   SizedBox(
+              //     width: double.infinity,
+              //     height: double.infinity,
+              //     child: Image.file(
+              //       File(controller.picture.value!.path),
+              //       fit: BoxFit.cover,
+              //     ),
+              //   ),
+              // Center(
+              //   child: Column(
+              //     crossAxisAlignment: CrossAxisAlignment.center,
+              //     mainAxisAlignment: MainAxisAlignment.center,
+              //     children: [
+              //       Text(
+              //         controller.isDetecting.value ? 'Terdeteksi' : 'Tidak Terdeteksi',
+              //         style: TextStyle(color: Colors.red, fontSize: 30.0),
+              //       ),
+              //       // if (controller.complete.value != null)
+              //       Text(
+              //         status,
+              //         style: TextStyle(color: Colors.red, fontSize: 30.0),
+              //       ),
+              //     ],
+              //   ),
+              // ),
+              FaceScanNavigation(),
+              FaceScanButton(),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
